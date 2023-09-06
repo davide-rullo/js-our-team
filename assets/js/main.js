bb7defcbce58e363f4746d985107cc5eb8fe3e1c
davide-rullo <davide.rullo01@gmail.com>
2023-09-06 14:43:25 +0200

    add
    added array of objects

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+
+/* creare un array di oggetti per rappresentare i membri del team. 
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.*/
+
+const teamMembers = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        picture: './assets/img/wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        picture: './assets/img/angela-caroll-chief-editor.jpg'
+    },
+
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        picture: './assets/img/walter-gordon-office-manager.jpg'
+    },
+
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        picture: './assets/img/angela-lopez-social-media-manager.jpg'
+    },
+
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        picture: './assets/img/scott-estrada-developer.jpg'
+    },
+
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        picture: './assets/img/barbara-ramos-graphic-designer.jpg'
+    }
+]
